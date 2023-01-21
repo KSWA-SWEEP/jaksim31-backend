890b51929255bd3e1ce261a5618d2659922de2b0
KIM-JOOHYUN <kjh99723@gmail.com>
2023-01-21 19:39:59 +0900

    SW-107 feat(diary): edit DiaryRepositoryTest
    - DiaryRepositoryTest 테스트 케이스 추가 및 수정
      > 추가) 일기 삭제, 오늘 일기 저장, 일기 전체 조회, 오늘 일기 조회, 사용자 일기 조건 검색
      > 수정)
        - 사용자 일기 전체 조회 : 조회 된 diary들의 userId가 검색 조건과 일치하는지 확인

--- src/test/java/com/sweep/jaksim31/domain/diary/DiaryRepositoryTest.java
+++ src/test/java/com/sweep/jaksim31/domain/diary/DiaryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.sweep.jaksim31.domain.diary;
 
 import com.sweep.jaksim31.domain.members.MemberRepository;
+import com.sweep.jaksim31.domain.members.Members;
 import com.sweep.jaksim31.dto.diary.DiaryInfoResponse;
 import com.sweep.jaksim31.util.ExecutionTimeTestExecutionListener;
 import org.junit.jupiter.api.*;
@@ -16,14 +17,25 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.data.domain.Sort.by;
+
+/**
+ * packageName :  com.sweep.jaksim31.domain.diary
+ * fileName : DiaryRepositoryTest
+ * author :  방근호
+ * date : 2023-01-20
+ * description : Diary Repository Test
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-01-20           방근호             최초 생성
+ * 2023-01-21           김주현             테스트 케이스 추가 및 수정
+ */
 
 @DataMongoTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -49,7 +61,6 @@ class DiaryRepositoryTest {
                 .build();
     }
 
-
     @BeforeAll
     void setup() {
         //given
@@ -73,6 +84,58 @@ class DiaryRepositoryTest {
 
     @Test
     @Order(2)
+    @DisplayName("일기 삭제 (Delete) - 63c0cb6f30dc3d547e3b88bb 사용자의 3번째 일기 삭제")
+    void deleteDiary() {
+        //given
+        Diary diary = diaryRepository.findAllByUserId("63c0cb6f30dc3d547e3b88bb").get(2);
+
+        //when
+        diaryRepository.delete(diary);
+
+        //then
+        assertEquals(diaryRepository.findAll().size(), 19);
+        for (Diary tmp : mongoTemplate.findAll(Diary.class, "diary")) {
+            System.out.println(tmp.toString());
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("오늘 일기 저장 (Create)")
+    void saveToday() {
+        //given
+        Diary diary = Diary.builder()
+                .userId("63c0cb6f30dc3d547e3b88bb")
+                .content("content" + " today")
+                .date(LocalDate.now())
+                .emotion("12")
+                .keywords(new String[]{"today"})
+                .thumbnail("thumbnail" + " today")
+                .build();
+
+        //when
+        diaryRepository.save(diary);
+
+        //then
+        assertEquals(diaryRepository.findAll().size(), 20);
+        assertTrue(diary.getContent().startsWith("content"));
+        assertEquals(Arrays.stream(diary.getKeywords()).count(), 1);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("일기 전체 조회 (Read)")
+    void findAll() {
+        //when
+        //then
+        assertEquals(diaryRepository.findAll().size(), 20);
+        for (Diary tmp : mongoTemplate.findAll(Diary.class, "diary")) {
+            System.out.println(tmp.toString());
+        }
+    }
+
+    @Test
+    @Order(5)
     @DisplayName("일기 조회 (Read) - ObjectId")
     void findById() {
         //given
@@ -89,18 +152,33 @@ class DiaryRepositoryTest {
     }
 
     @Test
-    @Order(3)
-    @DisplayName("일기 전체 조회 (Read) - User ObjectId")
+    @Order(6)
+    @DisplayName("사용자 일기 전체 조회 (Read) - User ObjectId")
     void findAllByUserId() {
         //then
         assertThat(diaryRepository.findAllByUserId("63c0cb6f30dc3d547e3b88bb").size())
                 .isEqualTo(20);
+        for (Diary tmp : diaryRepository.findAllByUserId("63c0cb6f30dc3d547e3b88bb")) {
+            assertThat(tmp.getUserId())
+                    .isEqualTo("63c0cb6f30dc3d547e3b88bb");
+        }
     }
 
     @Test
-    @Order(4)
-    @DisplayName("MongoTemplate")
+    @Order(7)
+    @DisplayName("오늘 일기 조회 (Read) - User ObjectId")
     void findDiaryByUserIdAndDate() {
+        //given
+        LocalDate today = LocalDate.now();
+        //then
+        assertThat(diaryRepository.findDiaryByUserIdAndDate("63c0cb6f30dc3d547e3b88bb", today.atTime(9,0)).get().getDate())
+                .isEqualTo(LocalDate.now().atTime(9,0));
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("MongoTemplate_사용자 전체 일기 조회(Paging)")
+    void findDiaryByUserId_MongoTemplate() {
         //given
         Pageable pageable = PageRequest.of(0 , 9, Sort.by("date"));
         Query query = new Query()
@@ -120,4 +198,32 @@ class DiaryRepositoryTest {
         }
 
     }
+
+    @Test
+    @Order(9)
+    @DisplayName("MongoTemplate_사용자 일기 조건 검색(Paging)")
+    void findDiary_MongoTemplate() {
+        //given
+        Pageable pageable = PageRequest.of(0 , 20, Sort.by("date"));
+        Query query = new Query()
+                .with(pageable)
+                .skip((long) pageable.getPageSize() * pageable.getPageNumber())
+                .limit(pageable.getPageSize());
+        query.addCriteria(Criteria.where("userId").is("63c0cb6f30dc3d547e3b88bb"));
+        query.addCriteria(Criteria.where("date").gte(LocalDate.parse("2023-01-10").atTime(9,0)).lte(LocalDate.parse("2023-01-15").atTime(9,0)));
+//        query.addCriteria(Criteria.where("emotion").is(""));
+
+        //when
+        List<DiaryInfoResponse> diaryInfoResponsesList = mongoTemplate.find(query, DiaryInfoResponse.class, "diary");
+
+        //then
+        assertThat(diaryInfoResponsesList.size()).isEqualTo(6);
+        int idx = 10;
+        for (DiaryInfoResponse diaryInfoResponse : diaryInfoResponsesList) {
+            assertThat(diaryInfoResponse.getEmotion()).isEqualTo(idx + "");
+            assertThat(diaryInfoResponse.getUserId()).isEqualTo("63c0cb6f30dc3d547e3b88bb");
+            idx++;
+        }
+    }
+
 }
