21d39e3584766e280d25f27223c8a9d0e4f92acb
banggeunho <panggeunho@gmail.com>
2023-02-06 05:26:19 +0900

    SW-146 modify swagger config & web config

--- src/main/java/com/sweep/jaksim31/config/SwaggerConfig.java
+++ src/main/java/com/sweep/jaksim31/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.sweep.jaksim31.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,14 @@ import org.springframework.context.annotation.Configuration;
  * DATE                 AUTHOR                NOTE
  * -----------------------------------------------------------
  * 2023-01-13           방근호             최초 생성
+ * 2023-02-06           방근호             테스트용 URL 추가
  *
  */
 
 @OpenAPIDefinition(
+        servers = {@Server(url = "http://localhost:8080"),
+                   @Server(url = "https://jaksim31.xyz")},
+
         info = @Info(
                 title = "Jaksim31 API",
                 description = "일기관리 서비스 \"작심상일\" API 명세서",
